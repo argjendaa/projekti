7cfbd639768954642f7db99a78e54bef26259d80
argjendaa <argjenda.istrefi@hotmail.com>
2025-01-24 21:30:18 +0100

    Delete projekti kryesor3/prov.js

--- projekti kryesor3/prov.js
+++
@@ -1,8 +0,0 @@
-function toggleTable() {
-    var table = document.getElementById("table-content");
-    if (table.style.display === "none") {
-      table.style.display = "table";
-    } else {
-      table.style.display = "none";
-    }
-  }
